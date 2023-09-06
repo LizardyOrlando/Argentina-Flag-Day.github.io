4d8de29dd3afd3809e45cc0f5d329bf72491f7a3
LizardyOrlando <97751586+LizardyOrlando@users.noreply.github.com>
2023-09-06 19:06:25 -0300

    Add files via upload

--- bandera.js
+++ bandera.js
@@ -6,3 +6,26 @@ const typed = new Typed('#titulo',{
     loop:false
 
 })
+
+const suma = document.getElementById('suma');// -> Uso del DOM donde llama el elemento en html
+const resta = document.getElementById('resta');
+const counter = document.getElementById('valor');// -> Uso del DOM donde llama el elemento en html
+let click = 0;// creación de variale tipo number
+
+suma.addEventListener('click',() =>{// Uso de Event Listener
+    click += 1;// suma a la variable en 1 cada vez que le den click
+    counter.textContent = click;//modifica el valor
+    if (click > 10) {// si el valor es mayor a 10 muestra el siguiente texto
+        counter.textContent = 'no se puede aumentar más'
+    }
+
+})
+
+resta.addEventListener('click',() =>{// Uso de Event Listener
+    click -= 1;// resta  a la variable 1 cada vez que le den click
+    counter.textContent = click;//modifica el valor
+    if (click < 0) {// si el valor es menor a cero muestra el texto
+        counter.textContent = 'no se puede descontar más'
+    }
+
+})
